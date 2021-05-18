63979718438f22fab6ec206abb53dda58612b545
왕호진 <kkgmco0825@gmail.com>
2021-05-19 02:18:02 +0900

    하우스서칭 추가

---
+++ happyhouse_vue/src/components/AptDetailListRow.vue
@@ -0,0 +1,26 @@
+<template>
+  <tr>
+    <td>{{ dong }}</td>
+    <td>
+      {{ aptName }}
+    </td>
+    <td>{{ dealAmount }}</td>
+    <td>{{ dealYear }}</td>
+    <td>{{ area }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "aptrow",
+  props: {
+    dong: { type: String },
+    aptName: { type: String },
+    dealAmount: { type: String },
+    dealYear: { type: String },
+    dealMonth: { type: String },
+    dealDay: { type: String },
+    area: { type: String },
+  },
+};
+</script>
---
+++ happyhouse_vue/src/components/AptListRow.vue
@@ -0,0 +1,20 @@
+<template>
+  <tr>
+    <td>{{ dong }}</td>
+    <td>
+      {{ aptName }}
+    </td>
+    <td>{{ buildYear }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "aptrow",
+  props: {
+    dong: { type: String },
+    aptName: { type: String },
+    buildYear: { type: String },
+  },
+};
+</script>
