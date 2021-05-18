638885a11ea4e3da3ce171bad8ec85cc547e90ea
Shimyeeun <yeeun7873@naver.com>
2021-05-19 02:19:53 +0900

    http-common 이것도 커밋해야되나...

--- happyhouse_vue/src/util/http-common.js
+++ happyhouse_vue/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: 'http://localhost:9999/vue/api',
+  baseURL: 'http://localhost:8080/happyhouse/',
   headers: {
     'Content-type': 'application/json',
   },
