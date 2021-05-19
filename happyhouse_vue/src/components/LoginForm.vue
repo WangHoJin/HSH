2a9ca6401bb28d7d2e4c9c1e07b1175e2a82beaf
Shimyeeun <yeeun7873@naver.com>
2021-05-19 13:30:55 +0900

    로그인...?

--- happyhouse_vue/src/components/LoginForm.vue
+++ happyhouse_vue/src/components/LoginForm.vue
@@ -71,8 +71,6 @@ export default {
         .then(({ data }) => {
           let msg = "로그인 실패";
           if (data === "success") {
-            // this.$store.commit("userid")
-            sessionStorage.setItem("ID", this.userid);
             msg = "로그인 성공";
             alert(msg);
             // this.$store.dispatch();
@@ -82,7 +80,7 @@ export default {
           } else {
             alert(msg);
           }
-          console.log(sessionStorage.getItem("ID"));
+
           this.$router.push("/");
         });
     },
