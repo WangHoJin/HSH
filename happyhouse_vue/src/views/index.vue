b515b9cc02c747624f1cf0b5687dc7325e2c8b24
왕호진 <kkgmco0825@gmail.com>
2021-05-24 01:59:14 +0900

    바 컬러 변경

--- happyhouse_vue/src/views/index.vue
+++ happyhouse_vue/src/views/index.vue
@@ -54,9 +54,9 @@
             <!-- Features -->
             <section class="box features">
               <h2 class="major"><span>A Major Heading</span></h2>
-              <ul>
-                <chartjs-bar></chartjs-bar>
-              </ul>
+              <div>
+                <chartjs-bar :labels="labels" :datasets="datasets"></chartjs-bar>
+              </div>
             </section>
           </div>
         </div>
@@ -76,7 +76,29 @@ export default {
     return {
       showcoffee: false,
       labels: ["January", "February", "March", "April", "May", "June", "July"],
-      dataset: [65, 59, 80, 81, 56, 55, 40],
+      datasets: [
+        {
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(255, 205, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(201, 203, 207, 0.2)",
+          ],
+          borderColor: [
+            "rgb(255, 99, 132)",
+            "rgb(255, 159, 64)",
+            "rgb(255, 205, 86)",
+            "rgb(75, 192, 192)",
+            "rgb(54, 162, 235)",
+            "rgb(153, 102, 255)",
+            "rgb(201, 203, 207)",
+          ],
+        },
+      ],
     };
   },
   methods: {
