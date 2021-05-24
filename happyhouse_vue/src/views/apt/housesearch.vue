0016ce7c914aeefc1d488dbc27880382a7fbd412
Shimyeeun <yeeun7873@naver.com>
2021-05-24 13:47:06 +0900

    housesearch 버튼 수정

--- happyhouse_vue/src/views/apt/housesearch.vue
+++ happyhouse_vue/src/views/apt/housesearch.vue
@@ -6,20 +6,17 @@
         <div class="row">
           <!-- filters start -->
           <div class="col-8 col-12-medium">
-            <div class="sorting-filters text-center mb-20 d-flex justify-content-center">
-              <button type="button" class="btn btn-dark mr-3" id="searchall" @click="searchAll">
+            <div class="sorting-filters text-center d-flex justify-content-center">
+              <button type="button" class="button mr-3" id="searchall" @click="searchAll">
                 전체 조회
               </button>
 
               <form class="form-inline ml-3 mr-3" id="frm" method="post" action="housesearch">
                 <input type="hidden" name="act" id="act" value="search" />
-                <label for="dongsearch" style="display: inline-block; margin-right: 10px"
-                  >동별 검색</label
-                >
 
                 <div class="form-group md">
                   <select class="form-control mr-2" id="sido" @change="getGugun($event)">
-                    <option value="0">선택</option>
+                    <option value="0">시/도</option>
                     <option
                       v-for="(sido, index) in sidos"
                       :key="`${index}_sidos`"
@@ -31,7 +28,7 @@
                 </div>
                 <div class="form-group mr-2 md-1">
                   <select class="form-control" id="gugun" @change="getDong($event)">
-                    <option value="0">선택</option>
+                    <option value="0">시/군/구</option>
                     <option
                       v-for="(gugun, index) in guguns"
                       :key="`${index}_guguns`"
@@ -43,7 +40,7 @@
                 </div>
                 <div class="form-group md-1">
                   <select class="form-control" id="dong" @change="getInfo($event)">
-                    <option value="0">선택</option>
+                    <option value="0">동</option>
                     <option
                       v-for="(dong, index) in dongs"
                       :key="`${index}_dongs`"
@@ -56,7 +53,7 @@
               </form>
               <button
                 type="button"
-                class="form-inline ml-3 btn btn-warning"
+                class="form-inline ml-3 button"
                 id="searchinterest"
                 @click="searchInterest"
               >
@@ -64,7 +61,7 @@
               </button>
               <button
                 type="button"
-                class="form-inline ml-3 btn btn-warning"
+                class="form-inline ml-3 button"
                 id="addinterest"
                 @click="addInterest"
               >
@@ -73,27 +70,32 @@
             </div>
           </div>
           <div class="col-4 col-12-medium">
-            <form id="frm" method="post" action="housesearch">
-              <div>
-                <input type="hidden" name="act" value="aptsearch" />
-                <label for="aptname" style="display: inline-block">아파트별 검색</label>
-                <input
-                  type="text"
-                  style="display: inline-block; width: 150px"
-                  id="apttext"
-                  v-model="aptname"
-                />
-                <button
-                  type="button"
-                  id="aptsearch"
-                  class="btn btn-dark"
-                  value="검색"
-                  @click="searchName"
-                >
-                  검색
-                </button>
-              </div>
-            </form>
+            <div class="form-group sorting-filters text-center d-flex justify-content-center">
+              <!-- <label for="aptname" style="display: inline-block">아파트별 검색</label> -->
+
+              <input
+                type="text"
+                style="
+                  display: inline-block;
+                  width: 80%;
+                  border: solid 2px #e7eae8;
+                  border-radius: 8px;
+                "
+                id="apttext"
+                placeholder="아파트 이름으로 검색하세요."
+                v-model="aptname"
+              />
+              <button
+                type="button"
+                id="aptsearch"
+                class="button"
+                value="검색"
+                @click="searchName"
+                style="margin: 0 0.5em"
+              >
+                검색
+              </button>
+            </div>
           </div>
           <!-- filters end -->
         </div>
